f00c1274b0f26046fe9c2d7d3c9ff1cdf4931f92
Josh Lemmond <joshualemmond@gmail.com>
2023-01-30 18:47:13 -0500

    adding seed data

--- models/Interests.js
+++ models/Interests.js
@@ -16,7 +16,7 @@ Interests.init(
             allowNull: false,
         },
         description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
     },
