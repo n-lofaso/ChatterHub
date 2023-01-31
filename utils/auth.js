e3acb7e6fb5198c3602d0956a90c28f2d0999144
n-lofaso <nicklofaso96@gmail.com>
2023-01-30 19:39:46 -0500

    Added code to auth.js and helpers.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,10 @@
+// use for a checkpoint for routes where the user needs to be logged in first
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY to use it for post date
-      return date.toLocaleDateString();
-    }
-};   
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY to use it for post date
+    return date.toLocaleDateString();
+  },
+};
