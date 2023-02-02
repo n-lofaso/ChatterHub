0cf87ef327de63bb22ae0798bf70fdea9295b0e0
Z404C <112136706+Z404C@users.noreply.github.com>
2023-02-02 18:55:16 -0500

    music route created

---
+++ controllers/music-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const {Post, Comment} = require ('../models');
+const withAuth = require('../utils/auth');
+
+//Music Route
+
+//GET all posts for Music page
+
+router.get('/music', withAuth, async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        include: [
+          {
+            model: Comment,
+          },
+        ],
+      });
+  
+      const posts = dbPostData.map((post) =>
+        post.get({ plain: true })
+      );
+  
+      res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.post('/music', withAuth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newComment);
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+  router.delete('/music/:id', withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+
+      const postData = await Post.destroy({
+        where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+        },
+      });
+  
+      if (!commentData||!postData) {
+        res.status(404).json({ message: 'Post not found' });
+        return;
+      }
+  
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
