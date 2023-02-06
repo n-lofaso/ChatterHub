1428f859dbe6833994e2e6a0233b49a6e7d14677
SimKat123 <simran12katuwal@gmail.com>
2023-02-05 19:52:01 -0500

    linked interests and post models, also made an env file

--- model/Post.js
+++ model/Post.js
@@ -1,39 +1,47 @@
-const { Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
 Post.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allownull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'posts'
-    }
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allownull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    interests_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "interests",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
 );
 
 module.exports = Post;
